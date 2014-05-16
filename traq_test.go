573266a6a67ad730a8b990213781ed0541ee77f2
Raphael Randschau <nicolai86@me.com>
2014-05-16 13:40:36 +0200

    update tests to please travis

--- traq_test.go
+++ traq_test.go
@@ -1,155 +1,159 @@
 package main
 
 import (
-	"os"
-	"testing"
-	"time"
 	"bytes"
 	"io"
-  "path"
+	"os"
+	"path"
+	"strings"
+	"testing"
+	"time"
 )
 
 // change traq env to use our fixtures
 func WithFakeEnv(block func()) {
-  oldEnv := os.Getenv("TRAQ_DATA_DIR")
-  path, _ := os.Getwd()
-  os.Setenv("TRAQ_DATA_DIR", path + "/fixtures")
+	oldEnv := os.Getenv("TRAQ_DATA_DIR")
+	path, _ := os.Getwd()
+	os.Setenv("TRAQ_DATA_DIR", path+"/fixtures")
 
-  block()
+	block()
 
-  os.Setenv("TRAQ_DATA_DIR", oldEnv)
+	os.Setenv("TRAQ_DATA_DIR", oldEnv)
 }
 
 // capture output written to os.Stdout and return it
 func CaptureStdout(block func()) string {
-  old := os.Stdout // keep backup of the real stdout
-  r, w, _ := os.Pipe()
-  os.Stdout = w
+	old := os.Stdout // keep backup of the real stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
 
-  block()
+	block()
 
-  outC := make(chan string)
-  // copy the output in a separate goroutine so printing can't block indefinitely
-  go func() {
-      var buf bytes.Buffer
-      io.Copy(&buf, r)
-      outC <- buf.String()
-  }()
+	outC := make(chan string)
+	// copy the output in a separate goroutine so printing can't block indefinitely
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
 
-  // back to normal state
-  w.Close()
-  os.Stdout = old
+	// back to normal state
+	w.Close()
+	os.Stdout = old
 
-  return <-outC
+	return <-outC
 }
 
 func TestDatesInMonth(t *testing.T) {
-  dates := DatesInMonth(1986, 9)
+	dates := DatesInMonth(1986, 9)
 
-  if len(dates) != 30 {
-    t.Errorf("expected 30 days in Sep 1986, got %v", len(dates))
-  }
+	if len(dates) != 30 {
+		t.Errorf("expected 30 days in Sep 1986, got %v", len(dates))
+	}
 
-  if dates[0].Weekday() != time.Monday {
-    t.Errorf("Started on a Monday, got %v", dates[0].Weekday())
-  }
+	if dates[0].Weekday() != time.Monday {
+		t.Errorf("Started on a Monday, got %v", dates[0].Weekday())
+	}
 
-  if dates[len(dates) - 1].Weekday() != time.Tuesday {
-    t.Errorf("Ended on a Tuesday, got %v", dates[len(dates) - 1].Weekday())
-  }
+	if dates[len(dates)-1].Weekday() != time.Tuesday {
+		t.Errorf("Ended on a Tuesday, got %v", dates[len(dates)-1].Weekday())
+	}
 }
 
 func TestPrintDate(t *testing.T) {
-  out := CaptureStdout(func() {
-    WithFakeEnv(func() {
-      PrintDate("example", time.Date(1986, 9, 3, 0, 0, 0, 0, time.UTC))
-    })
-  })
+	out := CaptureStdout(func() {
+		WithFakeEnv(func() {
+			PrintDate("example", time.Date(1986, 9, 3, 0, 0, 0, 0, time.UTC))
+		})
+	})
 
-  expected :=
-`Wed Sep 03 20:00:00 +0100 1986;#birth;comment
+	expected :=
+		`Wed Sep 03 20:00:00 +0100 1986;#birth;comment
 Wed Sep 03 21:45:33 +0100 1986;#chillout;
 Wed Sep 03 23:24:49 +0100 1986;stop;
 %%
 `
-  if out != expected {
-    t.Errorf("unexpected PrintDate output. Expected '%v' got '%v'", expected, out)
-  }
+	if out != expected {
+		t.Errorf("unexpected PrintDate output. Expected '%v' got '%v'", expected, out)
+	}
 }
 
 func TestEvaluateDate(t *testing.T) {
-  out := CaptureStdout(func() {
-    WithFakeEnv(func() {
-      EvaluateDate(ContentLoader, "example", time.Date(1986, 9, 3, 0, 0, 0, 0, time.UTC))
-    })
-  })
+	out := CaptureStdout(func() {
+		WithFakeEnv(func() {
+			EvaluateDate(ContentLoader, "example", time.Date(1986, 9, 3, 0, 0, 0, 0, time.UTC))
+		})
+	})
 
-  expected :=
-`1986-09-03
-#birth:1.7592
-#chillout:1.6544
-%%
-`
-
-  if out != expected {
-    t.Errorf("unexpected EvaluateDate output. Expected '%v' got '%v'", expected, out)
-  }
+	expectedLines := map[string]bool{
+		"1986-09-03":       false,
+		"#birth:1.7592":    false,
+		"#chillout:1.6544": false,
+	}
+	for _, line := range strings.Split(out, "\n") {
+		expectedLines[line] = true
+	}
+	for key, present := range expectedLines {
+		if !present {
+			t.Errorf("unexpected EvaluateDate output. Expected '%v', missing from '%v'", key, out)
+		}
+	}
 }
 
 func TestEntry(t *testing.T) {
-  expected := `Wed Sep 3 12:00:00 +0000 1986;#test;
+	expected := `Wed Sep 3 12:00:00 +0000 1986;#test;
 `
 
-  if entry := Entry(time.Date(1986, 9, 3, 12, 0, 0, 0, time.UTC), "#test"); entry != expected {
-    t.Errorf("got wrong entry. Expected '%v' got '%v'", expected, entry)
-  }
+	if entry := Entry(time.Date(1986, 9, 3, 12, 0, 0, 0, time.UTC), "#test"); entry != expected {
+		t.Errorf("got wrong entry. Expected '%v' got '%v'", expected, entry)
+	}
 }
 
 func TestWriteToFile(t *testing.T) {
-  startDate := time.Date(2013, 1, 3, 12, 30, 0, 0, time.UTC)
-  endDate := time.Date(2013, 1, 3, 13, 30, 0, 0, time.UTC)
+	startDate := time.Date(2013, 1, 3, 12, 30, 0, 0, time.UTC)
+	endDate := time.Date(2013, 1, 3, 13, 30, 0, 0, time.UTC)
 
-  WithFakeEnv(func() {
-    WriteToFile("example", startDate, "test")
+	WithFakeEnv(func() {
+		WriteToFile("example", startDate, "test")
 
-    filePath := FilePath("example", startDate)
-    out, _ := ContentLoader(filePath)
-    if len(out) != 2 {
-      t.Errorf("Expected different line count. Got %v", len(out))
-    }
+		filePath := FilePath("example", startDate)
+		out, _ := ContentLoader(filePath)
+		if len(out) != 2 {
+			t.Errorf("Expected different line count. Got %v", len(out))
+		}
 
-    if out[0] != "Thu Jan 3 12:30:00 +0000 2013;#test;" {
-      t.Errorf("Expected different first line. Got %v", out[0])
-    }
+		if out[0] != "Thu Jan 3 12:30:00 +0000 2013;#test;" {
+			t.Errorf("Expected different first line. Got %v", out[0])
+		}
 
-    WriteToFile("example", endDate, "stop")
-    out, _ = ContentLoader(filePath)
+		WriteToFile("example", endDate, "stop")
+		out, _ = ContentLoader(filePath)
 
-    if len(out) != 3 {
-      t.Errorf("Expected different line count. Got %v", len(out))
-    }
-    if out[1] != "Thu Jan 3 13:30:00 +0000 2013;stop;" {
-      t.Errorf("Expected different stop line. Got %v", out[1])
-    }
+		if len(out) != 3 {
+			t.Errorf("Expected different line count. Got %v", len(out))
+		}
+		if out[1] != "Thu Jan 3 13:30:00 +0000 2013;stop;" {
+			t.Errorf("Expected different stop line. Got %v", out[1])
+		}
 
-    os.RemoveAll(path.Dir(filePath))
-  })
+		os.RemoveAll(path.Dir(filePath))
+	})
 }
 
 func TestRunningLoader(t *testing.T) {
-  startDate := time.Date(2013, 1, 3, 12, 30, 0, 0, time.UTC)
+	startDate := time.Date(2013, 1, 3, 12, 30, 0, 0, time.UTC)
 
-  WithFakeEnv(func() {
-    WriteToFile("example", startDate, "test")
+	WithFakeEnv(func() {
+		WriteToFile("example", startDate, "test")
 
-    filePath := FilePath("example", startDate)
-    out, _ := RunningLoader(filePath)
-    if len(out) != 3 {
-      t.Errorf("Expected different line count. Got %v", len(out))
-    }
+		filePath := FilePath("example", startDate)
+		out, _ := RunningLoader(filePath)
+		if len(out) != 3 {
+			t.Errorf("Expected different line count. Got %v", len(out))
+		}
 
-    os.RemoveAll(path.Dir(filePath))
-  })
+		os.RemoveAll(path.Dir(filePath))
+	})
 }
 
 func TestFilePath(t *testing.T) {
